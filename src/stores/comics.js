0906970cd3ef98932d0bca6d71c21e948d03439f
efetekce <efete_kce@proton.me>
2023-06-18 18:27:48 +0300

    some more changes

--- src/stores/comics.js
+++ src/stores/comics.js
@@ -8,6 +8,8 @@ export const useComicsStore = defineStore('comics', () => {
   const favCount = ref(0)
   const list = ref([])
   const favList = ref([])
+  const apiKey = import.meta.env.VITE_API_KEY
+  const hash = import.meta.env.VITE_HASH
 
   //getters
   // const doubleCount = computed(() => count.value * 2)
@@ -16,7 +18,7 @@ export const useComicsStore = defineStore('comics', () => {
 
   async function fetchData() {
     const bigData = await axios.get(
-      'http://gateway.marvel.com/v1/public/comics?orderBy=focDate&limit=50&ts=1&apikey=fc35d395c01a6d850fa395f2d32a9ddc&hash=1f29a8967f93f0c2110f9e08f163d935'
+      `http://gateway.marvel.com/v1/public/comics?orderBy=focDate&limit=50&ts=1&apikey=${apiKey}&hash=${hash}`
     )
     // console.log(bigData.data.data.results)
     list.value = bigData.data.data.results
